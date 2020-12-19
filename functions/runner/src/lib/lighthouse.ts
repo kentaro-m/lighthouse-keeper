0ae7c5ec08985e6e17c529f1f56a624d8c6733d9
kentaro-m <kentaro.m811@gmail.com>
2020-12-19 17:07:01 +0900

    Adjust configuration to run lighthouse

--- functions/runner/src/lib/lighthouse.ts
+++ functions/runner/src/lib/lighthouse.ts
@@ -1,4 +1,5 @@
 const lighthouse = require('lighthouse')
+const constants = require('../config/constants')
 const chromium = require('chrome-aws-lambda')
 const puppeteer = require('puppeteer-core')
 
@@ -8,12 +9,23 @@ export async function runLighthouse(url: string) {
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath,
     headless: chromium.headless,
+    ignoreHTTPSErrors: true,
   })
 
   const result = await lighthouse(url, {
     logLevel: 'info',
-    onlyCategories: ['performance'],
     port: (new URL(browser.wsEndpoint())).port,
+  }, {
+    extends: 'lighthouse:default',
+    settings: {
+      onlyCategories: ['performance'],
+      // maxWaitForFcp: 15 * 1000,
+      // maxWaitForLoad: 35 * 1000,
+      formFactor: 'desktop',
+      throttling: constants.throttling.desktopDense4G,
+      screenEmulation: constants.screenEmulationMetrics.desktop,
+      emulatedUserAgent: constants.userAgents.desktop,
+    },
   });
   
   await browser.close();
